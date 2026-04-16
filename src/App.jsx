0430e4c9adcde425169b9bd536597bb0d9d0534f
Alan Correa <am.correagarcia@gmail.com>
2026-04-15 19:39:45 -0600

    Creación  del formulario chat básico

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { Form } from './components/Form'
+import { useForm } from 'react-hook-form'
 
 export const App = () => {
+  const { register, handleSubmit } = useForm()
+
+  const handlePregunta = (data) => {
+    console.log(data)
+  }
+
   return (
-    <Form />
+    <>
+      <h1>ChatBot</h1>
+      <form onSubmit={handleSubmit(handlePregunta)}>
+        <input
+          type='text'
+          {...register('userInput')}
+          className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+        <button className='w-full py-2 rounded transition cursor-ponter bg-blue-600 text-white hover:bg-blue-700'>Preguntar</button>
+      </form>
+    </>
   )
 }
